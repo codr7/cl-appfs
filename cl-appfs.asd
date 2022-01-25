ddb787e357c66b53fe1aa518bf08c65ca8c1fdfc
Andreas Nilsson <andreas.nilsson@jayway.com>
2022-01-25 18:38:41 +0100

    Add conn fns.

---
+++ cl-appfs.asd
@@ -0,0 +1,13 @@
+(asdf:defsystem cl-appfs
+  :name "cl-appfs"
+  :version "1"
+  :maintainer "codr7"
+  :author "codr7"
+  :description "in-app directory/shell"
+  :licence "MIT"
+  :build-operation "asdf:program-op"
+  :build-pathname "clappfs"
+  :entry-point "cl-appfs:main"
+  :depends-on ("cffi" "local-time")
+  :serial t
+  :components ((:file "pg")))
--- pg.lisp
+++ pg.lisp
@@ -1,8 +1,33 @@
 (defpackage cl-appfs-pg
-  (:use cl cffi)
+  (:use cffi cl)
   (:export))
 
 (in-package cl-appfs-pg)
 
+(define-foreign-library libpq (t (:default "/usr/local/opt/libpq/lib/libpq")))
 
+(use-foreign-library libpq)
 
+(defctype PGconn :pointer)
+
+(defcenum ConnStatusType :CONNECTION_OK :CONNECTION_BAD)
+
+;; PGconn *PQconnectdb(const char *conninfo)
+(defcfun "PQconnectdb" PGconn (conninfo :string))
+
+;; void PQfinish(PGconn *conn)
+(defcfun "PQfinish" :void (conn PGconn))
+
+;;ConnStatusType PQstatus(const PGconn *conn)
+(defcfun "PQstatus" ConnStatusType (conn PGconn))
+
+(defun test ()
+  (let* ((c (PQconnectdb ""))
+	 (s (PQstatus c)))
+    (format t "status: ~a~%" s)
+    (PQfinish c)))
+
+;;PQsendQueryParams
+;;PQsendPrepare
+;;PQsendQueryPrepared
+;;PQgetResult
