da0c61a8f802c75c0883fb6b9b3bfe82b5bec11f
Andreas Nilsson <andreas.nilsson@jayway.com>
2022-01-27 04:16:06 +0100

    Added folders.

--- fs.lisp
+++ fs.lisp
@@ -20,29 +20,43 @@
 (defmethod cl-appfs-db:model-table ((self user))
   (users *db*))
 
+(defstruct (folder (:include struct-model))
+  (name "" :type string)
+  (created-at (error "missing created-at") :type timestamp))
+
 (defclass db ()
   ((connection :initarg :connection :reader connection)
    (users-name :reader users-name)
    (users-created-at :reader users-created-at)
    (users-last-login-at :reader users-last-login-at)
    (users-last-logout-at :reader users-last-logout-at)
-   (users :initarg :users :reader users)))
+   (users :initarg :users :reader users)
+   (folders-name :reader folders-name)
+   (folders-created-at :reader folders-created-at)
+   (folders :initarg :folders :reader folders)))
 
 (defun new-db (&key (connection *connection*))
   (let ((*db* (make-instance 'db :connection connection)))
-    (with-slots (users-name users-created-at users-last-login-at users-last-logout-at users) *db*
+    (with-slots (folders folders-name folders-created-at
+		 users users-name users-created-at users-last-login-at users-last-logout-at) *db*
       (setf users-name (new-string-column 'name))
       (setf users-created-at (new-timestamp-column 'created-at))
       (setf users-last-login-at (new-timestamp-column 'last-login-at))
       (setf users-last-logout-at (new-timestamp-column 'last-logout-at))
       (setf users (new-table 'users '(name) (list users-name users-created-at users-last-login-at users-last-logout-at)))
 
+      (setf folders-name (new-string-column 'name))
+      (setf folders-created-at (new-timestamp-column 'created-at))
+      (setf folders (new-table 'folders '(name) (list folders-name folders-created-at )))
+
       (table-drop users)
       (unless (table-exists? users)
 	(table-create users)
 	(let ((admin (new-user "admin")))
-	  (model-store admin))))
-    *db*))
+	  (model-store admin)))
+      (unless (table-exists? folders)
+	(table-create folders))
+    *db*)))
 
 (defmacro with-db ((&rest args) &body body)
   `(with-connection (,@args)
